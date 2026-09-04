adb61960f5d86200504a746436d118849200e494
iman shirani <imanshirani@gmail.com>
2026-09-04 03:19:51 -0400

    Add files via upload

--- camera_rig/constants.py
+++ camera_rig/constants.py
@@ -1,4 +1,4 @@
-VERSION    = "0.0.1"
+VERSION    = "0.0.2"
 AUTHOR     = "Iman Shirani"
 GITHUB_URL = "https://github.com/imanshirani/Camera-Rig-Controller-PRO"
 PAYPAL_URL = "https://www.paypal.com/donate/?hosted_button_id=LAMNRY6DDWDC4"
